209d67219fa9bb001767e949761bc3ae98c01b48
Arinaky <2ruslanek2@gmail.com>
2022-04-08 17:35:56 +0500

    Import project

---
+++ src/main/java/webapp/Calc.java
@@ -0,0 +1,54 @@
+package webapp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Calc", urlPatterns="/Calc")
+public class Calc extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
+		Calc.setAsRequestAttributesAndCalculate(request);
+		
+		request.getRequestDispatcher("/Result.jsp").forward(request, response);
+	}
+	
+	private static class RequestCalc {
+		private final String radius_calc;
+		private final String height_calc;
+		private double result;
+		
+		public RequestCalc(String radius, String height) {
+			this.radius_calc = radius;
+			this.height_calc = height;
+		}
+		
+		public static RequestCalc fromRequestParameters(HttpServletRequest request) {
+			return new RequestCalc(
+			request.getParameter("radius"),
+			request.getParameter("height"));
+		}
+		
+		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
+			
+			request.setAttribute("radius", radius_calc);
+			request.setAttribute("height", height_calc);
+			int radius_try;
+			int height_try;
+			try {
+				radius_try= Integer.valueOf(radius_calc);
+				height_try= Integer.valueOf(radius_calc);
+			} catch (NumberFormatException e) {
+				radius_try=0;
+				height_try=0;
+			}
+			result = Math.PI*Math.pow(radius_try, 2)*height_try/3.0;
+			request.setAttribute("result", result);
+		}
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" isELIgnored="false" %>
+<!DOCTYPE html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Калькулятор - объём конуса</title>
+</head>
+<body>
+	<h1>Калькулятор = объем конуса</h1>
+	<h2><em>Разработано DreamTeam</em></h2>
+	<form action="${pageContext.request.contextPath}/Calc" method="post">
+		<label for="radius">Радиус: </label>
+		<br>
+		<input type="text" name="radius" id="radius" value="${radius}">
+		<br>
+		<label for="height">Высота: </label>
+		<br>
+		<input type="text" name="height" id="height" value="${height}">
+		<br>
+		<input type="submit" name="sign" value="Calculate">
+	</form>
+</body>
+</html>
