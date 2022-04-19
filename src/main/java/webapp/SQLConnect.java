a3899d6a623335ed34e9d248b6d6caac18d23de3
Arinaky <2ruslanek2@gmail.com>
2022-04-19 23:19:08 +0500

    Fix issues

--- src/main/java/webapp/SQLConnect.java
+++ src/main/java/webapp/SQLConnect.java
@@ -39,39 +39,6 @@ public class SQLConnect {
         catch (SQLException e) { System.out.println("Ошибка! Не удалось создать таблицу!"); }
     }
 
-    // Добавление данных в БД
-    public static void writeDB() {
-        if (connection != null) {
-            String name;
-            String state;
-            input.nextLine();
-            System.out.print("Введите название аниме: ");
-            name = input.nextLine();
-            System.out.print("Введите статус аниме: ");
-            state = input.nextLine();
-            try {
-                statement.execute("INSERT INTO 'AnimeTitles' ('name', 'state') VALUES ('" + name + "', '" + state + "'); ");
-                System.out.println("Аниме добавлено!"); }
-            catch (SQLException e) { System.out.println("Ошибка! Не удалось добавить данные в базу данных!"); }
-        } else { System.out.println("Ошибка! Подключитесь к базе данных сперва!"); }
-    }
-
-    // Чтение данных в БД
-    public static void readDB() {
-        if (statement != null) {
-            try {
-                resultSet = statement.executeQuery("SELECT * FROM AnimeTitles");
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String name = resultSet.getString("name");
-                    String state = resultSet.getString("state");
-                    System.out.println(id + " - " + name + " - " + state);
-                }
-                System.out.println("Таблица выведена!"); }
-            catch (SQLException e) { System.out.println("Ошибка! Не удалось прочесть данные в базе данных!"); }
-        } else { System.out.println("Ошибка! Подключитесь к базе данных сперва!"); }
-    }
-
     // Проверка на число
     public static boolean isNumeric(String str) {
         try { Double.parseDouble(str); return true; }
