0460d04bbd28147d5a5e85398bf74dfc3df3785e
Arinaky <2ruslanek2@gmail.com>
2022-04-20 17:15:18 +0500

    Fix calc 1

--- src/main/java/webapp/Calc.java
+++ src/main/java/webapp/Calc.java
@@ -80,7 +80,7 @@ public class Calc extends HttpServlet {
 
 			request.setAttribute("clean_services", clean_services);
 			request.setAttribute("price", order.getPrice());
-			request.setAttribute("result", SQLConnect.getResult());
+			request.setAttribute("result", order.getError());
 		}
 	}
 }
