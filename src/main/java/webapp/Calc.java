4200df530d1c820487788a9f85dd3d81237da24d
Arinaky <2ruslanek2@gmail.com>
2022-05-01 19:36:19 +0500

    Fix changing DB value 9

--- src/main/java/webapp/Calc.java
+++ src/main/java/webapp/Calc.java
@@ -90,6 +90,7 @@ public class Calc extends HttpServlet {
 			request.setAttribute("clean_services", clean_services);
 			request.setAttribute("price", order.getPrice());
 			request.setAttribute("number", number);
+			request.setAttribute("result", SQLConnect.getResult());
 			order.GeneratePDF();
 			SQLConnect.Close();
 		}
