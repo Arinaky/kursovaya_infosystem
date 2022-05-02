5df51d230951bd75c9c7af4239200953af1964d6
Arinaky <2ruslanek2@gmail.com>
2022-05-02 20:21:00 +0500

    Trying to add alert messages with JS 3

--- src/main/java/webapp/test.java
+++ src/main/java/webapp/test.java
@@ -1,5 +1,9 @@
 package webapp;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @WebServlet(name="test", urlPatterns="/test")
 public class test extends HttpServlet {
@@ -16,6 +24,19 @@ public class test extends HttpServlet {
         welcome.setAsRequestAttributesAndCalculate(request);
 		String message = (String)request.getAttribute("alertMsg");
 		JOptionPane.showMessageDialog(null, "hello");
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("JavaScript");
+		try {
+			engine.eval(Files.newBufferedReader(Paths.get(getClass().getResource("js/message.js").toURI()), StandardCharsets.UTF_8));
+			Invocable inv = (Invocable) engine;
+			inv.invokeFunction("message");
+		} catch (ScriptException e) {
+			throw new RuntimeException(e);
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
 		request.getRequestDispatcher("/welcome.jsp").forward(request, response);
 		JOptionPane.showMessageDialog(null, "world");
 	}
---
+++ src/main/resources/js/message.js
@@ -0,0 +1,4 @@
+function message() {
+var msg = "<%=message%>";
+alert(msg);
+}
